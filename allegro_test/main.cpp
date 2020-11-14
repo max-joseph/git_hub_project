f56d425514545a5d14cf0df783cb7a5402f1ebf4
max-joseph <73023553+max-joseph@users.noreply.github.com>
2020-11-14 10:01:35 +0330

    func arai va point ro tarif kardam arai tarif moteghaier baraie araie hast va pont ham mokhtasat shansi mide

--- allegro_test/main.cpp
+++ allegro_test/main.cpp
@@ -2,6 +2,19 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 using namespace std;
+
+int arai(int x[100], int a){
+    for (int i = 0; i < 100; i++) {
+        x[i] = a;
+    }
+    return 0;
+}
+void point(int x, int y){
+    x = rand() % 990;
+    y = rand() % 990;
+}
+
+
 int main(int argc, char **argv)
 {
     if (!al_init()) {
@@ -14,9 +27,120 @@ int main(int argc, char **argv)
         return 1;
     }
     al_clear_to_color(al_map_rgb(200, 200, 200));
-    ALLEGRO_COLOR green = al_map_rgb(44, 117, 255);
-    ALLEGRO_COLOR gray = al_map_rgb(0,100,0);
+    ALLEGRO_COLOR blue = al_map_rgb(44, 117, 255);
+    ALLEGRO_COLOR red = al_map_rgb(0,50,0);
     
+    
+//    snake
+  
+    al_init_primitives_addon();
+    al_install_keyboard();
+    ALLEGRO_BITMAP *bm = al_create_bitmap(1000, 1000);
+    
+    int x[100], y[100];
+    int a = 3, b = a - 1, e = 0, c = rand() % 990, d = rand() % 990;
+    arai(x, 450);
+    arai(y, 450);
+    bool done = false;
+    bool draw = false;
+    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+    ALLEGRO_EVENT ev;
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    while (!done) {
+        al_flip_display();
+        al_draw_filled_circle(c, d, 20, red);
+        if (e % 60 == 0) {
+            point(c, d);
+        }
+        al_draw_rectangle(0, 0, 1000, 1000, blue, 15);
+        al_wait_for_event(event_queue, &ev);
+        if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            switch (ev.keyboard.keycode) {
+                case ALLEGRO_KEY_DOWN:
+                    for (int i = 0; i < b; i++) {
+                        x[b - i] = x[b - 1 - i];
+                        y[b - i] = y[b - 1 - i];
+                    }
+                    y[0] = y[0] + 35;
+                    break;
+                case ALLEGRO_KEY_UP:
+                    for (int i = 0; i < b; i++) {
+                        x[b - i] = x[b - 1 - i];
+                        y[b - i] = y[b - 1 - i];
+                    }
+                    y[0] = y[0] - 35;
+                    break;
+                case ALLEGRO_KEY_RIGHT:
+                    for (int i = 0; i < b; i++) {
+                        x[b - i] = x[b - 1 - i];
+                        y[b - i] = y[b - 1 - i];
+                    }
+                    x[0] = x[0] + 35;
+                    break;
+                case ALLEGRO_KEY_LEFT:
+                    for (int i = 0; i < b; i++) {
+                        x[b - i] = x[b - 1 - i];
+                        y[b - i] = y[b - 1 - i];
+                    }
+                    x[0] = x[0] - 35;
+                    break;
+                case ALLEGRO_KEY_ESCAPE:
+                    done = true;
+                    break;
+                case ALLEGRO_KEY_SPACE:
+                    draw = done;
+            }
+        }
+        
+        al_flip_display();
+        al_clear_to_color(al_map_rgb(200, 200, 200));
+        al_flip_display();
+        if (draw == done) {
+            for (int i = 0; i < 3; i++) {
+                al_draw_filled_rectangle(x[i], y[i], x[i] + 30, y[i] +30, blue);
+            }
+        }
+        e++;
+    }
+    
+
+
+    
+
+
+    al_flip_display();
+    al_rest(3.00);
+    al_destroy_display(Display);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    rosh bezani sabz she
 //    for (int i = 0; i <= 10; i++) {
 //        for (int j = 0; j <= 10; j++) {
@@ -104,95 +228,3 @@ int main(int argc, char **argv)
 //        }
 //        }
     
-    
- 
-    
-    
-    
-    
-//    snake
-    al_init_primitives_addon();
-    al_install_keyboard();
-    int x[3], y[3];
-    int a = 3, b = a - 1, e = 0, c = rand() % 1000 , d = rand() % 1000;
-    for (int i = 0; i < 3; i++) {
-        x[i] = 450;
-        y[i] = 450;
-    }
-    bool done = false;
-    bool draw = false;
-    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-    ALLEGRO_EVENT ev;
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-    while (!done) {
-        al_flip_display();
-        al_draw_filled_circle(c, d, 20, gray);
-        if (e % 60 == 0) {
-            c = rand() % 1000;
-            d = rand() % 1000;
-        }
-        al_draw_rectangle(0, 0, 1000, 1000, green, 15);
-        al_wait_for_event(event_queue, &ev);
-        if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-            switch (ev.keyboard.keycode) {
-                case ALLEGRO_KEY_DOWN:
-                    for (int i = 0; i < b; i++) {
-                        x[b - i] = x[b - 1 - i];
-                        y[b - i] = y[b - 1 - i];
-                    }
-                    y[0] = y[0] + 60;
-                    break;
-                case ALLEGRO_KEY_UP:
-                    for (int i = 0; i < b; i++) {
-                        x[b - i] = x[b - 1 - i];
-                        y[b - i] = y[b - 1 - i];
-                    }
-                    y[0] = y[0] - 60;
-                    break;
-                case ALLEGRO_KEY_RIGHT:
-                    for (int i = 0; i < b; i++) {
-                        x[b - i] = x[b - 1 - i];
-                        y[b - i] = y[b - 1 - i];
-                    }
-                    x[0] = x[0] + 60;
-                    break;
-                case ALLEGRO_KEY_LEFT:
-                    for (int i = 0; i < b; i++) {
-                        x[b - i] = x[b - 1 - i];
-                        y[b - i] = y[b - 1 - i];
-                    }
-                    x[0] = x[0] - 60;
-                    break;
-                case ALLEGRO_KEY_ESCAPE:
-                    done = true;
-                    break;
-                case ALLEGRO_KEY_SPACE:
-                    draw = done;
-            }
-        }
-        
-        al_flip_display();
-        al_clear_to_color(al_map_rgb(200, 200, 200));
-        al_flip_display();
-        if (draw == done) {
-            for (int i = 0; i < 3; i++) {
-                al_draw_filled_rectangle(x[i], y[i], x[i] + 50, y[i] +50, green);
-            }
-        }
-        e++;
-    }
-    al_destroy_display(Display);
-
-    
-    
-    
-    
-
-
-    
-
-    
-    
-    
-    return 0;
-}
